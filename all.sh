5331adfc8334493319e23f69e45610394bd1d1ec
Indramal Wansekara <wmindramalw@gmail.com>
2023-09-12 07:40:44 +0530

    Create all.sh

---
+++ all.sh
@@ -0,0 +1,26 @@
+ #!/bin/bash 
+
+echo "#### Installation stating... ####"
+
+sudo su -
+
+sudo apt update && sudo apt upgrade -y
+
+echo "#### Webmin Installation ####"
+
+curl -o setup-repos.sh https://raw.githubusercontent.com/webmin/webmin/master/setup-repos.sh
+sh setup-repos.sh
+
+apt-get install webmin --install-recommends
+
+service webmin start
+
+sudo systemctl enable webmin
+
+sudo /usr/share/webmin/changepass.pl /etc/webmin root 0776046720
+
+echo "#### Cyberpanel Installation ####"
+
+sh <(curl https://cyberpanel.net/install.sh || wget -O - https://cyberpanel.net/install.sh)
+
+echo "#### Installation end ####"
